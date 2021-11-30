0d7285d9cc94679056f6b0bb3d0457fe5904ee02
Mickael Patron <mickael_patron@hotmail.com>
2021-11-30 13:37:59 +0100

    hey, look over there!

--- install-replica.sh
+++ install-replica.sh
@@ -4,7 +4,7 @@ set -x
 # export IPA_SERVER_NAME=node1
 # export IPA_SERVER_DOMAIN=jobjects.net
 # export IPA_SERVER_IP=192.168.56.141
-# export IPA_SERVER_NTP=ntp1.net-courrier.extra.laposte.fr
+# export IPA_SERVER_NTP=0.fr.pool.ntp.org
 
 ntpdate -su $IPA_SERVER_NTP
 
